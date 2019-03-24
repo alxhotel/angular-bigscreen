6040387dfe2d48a6896bf63672f40955c19fda63
Alex <alxmorais8@msn.com>
2019-03-24 19:46:01 +0100

    Add library

---
+++ projects/angular-bigscreen/src/lib/bigscreen.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+import { DocumentRef } from './document-ref';
+
+import { BigScreenService } from './bigscreen.service';
+
+@NgModule({
+	declarations: [
+	],
+	imports: [
+	],
+	providers: [
+		DocumentRef,
+		BigScreenService
+	],
+	exports: [
+	]
+})
+export class BigScreenModule {
+}
---
+++ projects/angular-bigscreen/src/lib/document-ref.ts
@@ -0,0 +1,10 @@
+export class DocumentRef {
+
+	/**
+	 * Access the native document.
+	 */
+	public get nativeDocument(): Document {
+		return document;
+	}
+
+}
