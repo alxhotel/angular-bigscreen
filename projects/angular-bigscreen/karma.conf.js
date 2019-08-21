53b38e57325a17e4aa79d5db98a6058a763b6373
Alex <alxmorais8@msn.com>
2019-08-22 01:23:48 +0200

    Update karma.conf.js

--- projects/angular-bigscreen/karma.conf.js
+++ projects/angular-bigscreen/karma.conf.js
@@ -30,3 +30,6 @@ module.exports = function (config) {
     restartOnFileChange: true
   });
 };
+
+process.env.NO_PROXY = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
+process.env.no_proxy = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
